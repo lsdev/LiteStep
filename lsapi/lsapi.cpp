0ba675748c97baed532cb4bd9fe3a60de9d72676
ilmcuts <ilmcuts@lsdev.org>
2003-01-01 15:29:09 +0000

    central definition of main window class/title

--- lsapi/lsapi.cpp
+++ lsapi/lsapi.cpp
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "lsapi.h"
 
 #include "../litestep/BangManager.h"
+#include "../litestep/litestep.h"
 #include "BangCommand.h"
 #include "SettingsManager.h"
 #include "bangs.h"
@@ -35,8 +36,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../utility/safestr.h" // Always include last in cpp file
 
 extern const char rcsRevision[];
-const char rcsRevision[] = "$Revision: 1.2 $"; // Our Version
-const char rcsId[] = "$Id: lsapi.cpp,v 1.2 2002/12/07 11:05:21 ilmcuts Exp $"; // The Full RCS ID.
+const char rcsRevision[] = "$Revision: 1.3 $"; // Our Version
+const char rcsId[] = "$Id: lsapi.cpp,v 1.3 2003/01/01 15:29:09 ilmcuts Exp $"; // The Full RCS ID.
 
 extern SettingsManager *gSettingsManager = NULL;
 
@@ -97,7 +98,7 @@ HWND LSAPIInit::GetLiteStepWnd()
 {
 	if (!m_hLiteStepWnd)
 	{
-		m_hLiteStepWnd = FindWindow("TApplication", "LiteStep");
+		m_hLiteStepWnd = FindWindow(szMainWindowClass, szMainWindowTitle);
 	}
 	return m_hLiteStepWnd;
 }
