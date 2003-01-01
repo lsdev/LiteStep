5b9c0a29e13d8825571f027725dccc0f5d6f3f34
ilmcuts <ilmcuts>
2003-01-01 14:32:32 +0000

    moved stringicmp to a separate file

--- utility/stringutility.h
+++ utility/stringutility.h
@@ -2,53 +2,40 @@
 This is a part of the LiteStep Shell Source code.
 
 Copyright (C) 1997-2002 The LiteStep Development Team
-
+  
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
-
+    
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
+      
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-#ifndef __BANGMANAGER_H
-#define __BANGMANAGER_H
+*/ 
+/****************************************************************************
+****************************************************************************/
+#ifndef __UTILITY_STRINGUTILITY_H
+#define __UTILITY_STRINGUTILITY_H
 
-#include "../utility/common.h"
-#include "../lsapi/BangCommand.h"
-#include "../utility/criticalsection.h"
-#include "../utility/stringutility.h"
-#include <map>
 #include <string>
 
-using namespace std;
-
-typedef map<string, Bang*, stringicmp> BangMap;
-
-class BangManager
+//
+// stringicmp
+//
+// provides case insensitive string comparison for std::map
+//
+struct stringicmp
 {
-private:
-	BangMap bang_map;
-	CRITICAL_SECTION cs;
-
-public:
-	BangManager();
-	virtual ~BangManager();
-
-	BOOL AddBangCommand(LPCSTR pszName, Bang *pbbBang);
-	BOOL RemoveBangCommand(LPCSTR pszName);
-	Bang* GetBangCommand(LPCSTR pszName);
-	void ClearBangCommands();
-	BOOL ExecuteBangCommand(LPCSTR pszName, HWND hCaller, LPCSTR pszParams);
-	UINT GetBangCommandNames(LPCSTR pszNames, UINT nSize);
-	STDMETHOD(get_Count)(PLONG plCount);
-
+    bool operator()(const std::string &s1, const std::string &s2) const
+    {
+        return (stricmp(s1.c_str(), s2.c_str()) < 0);
+    }
 };
 
-#endif // __BANGMANAGER_H
+
+#endif // __UTILITY_STRINGUTILITY_H
