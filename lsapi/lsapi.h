add38b02a3ada2e9b39240c7504f01c2d00acb6f
ilmcuts <ilmcuts>
2003-05-01 18:12:48 +0000

    reduced header dependency

--- lsapi/lsapi.h
+++ lsapi/lsapi.h
@@ -21,10 +21,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define  __LSAPI_H
 
 #include "../utility/common.h"
-#include <stdio.h>
 
 #include "lsapidefines.h"
-#include "../utility/wharfdata.h"
 #include "../litestep/resource.h"
 #include "../lsapi/lsmultimon.h"
 
