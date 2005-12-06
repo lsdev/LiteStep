c09cf0c0e3f267eb60e9b589008c2cc8be174d6b
jugg <jugg@lsdev.org>
2005-12-06 18:51:35 +0000

    Set no MFC dep flag.

--- litestep/resource.h
+++ litestep/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by litestep.rc
 //
 #define IDS_LITESTEP_ERROR1             1
@@ -51,6 +51,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1020
