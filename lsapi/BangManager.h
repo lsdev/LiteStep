70d8e0cd2cf03ad848c4a415525306462167d01b
maduin <maduin@lsdev.org>
2005-12-12 22:47:22 +0000

    Added doc comments, added Doxyfile for doxygen

--- lsapi/BangManager.h
+++ lsapi/BangManager.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <string>
 
 /**
- * Maintains a list of bang commands.
+ * Manages bang commands.
  */
 class BangManager
 {
