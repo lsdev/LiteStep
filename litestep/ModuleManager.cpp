47c20a12395bf00ac0d8073b8bcff0590658134f
jugg <jugg>
2008-04-24 00:47:03 +0000

    Removed unecessary inclusion of macros.h.

--- litestep/ModuleManager.cpp
+++ litestep/ModuleManager.cpp
@@ -20,7 +20,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /****************************************************************************
 ****************************************************************************/
 #include "ModuleManager.h"
-#include "../utility/macros.h"
 #include <algorithm>
 #include <vector>
 #include "../utility/core.hpp"
