f7107170bcce00d93ae80cb4ec4fbdd45e420026
jugg <jugg>
2008-12-31 00:45:47 +0000

    Added default constructor and destructor.

--- litestep/MessageManager.cpp
+++ litestep/MessageManager.cpp
@@ -22,6 +22,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "MessageManager.h"
 
 
+MessageManager::MessageManager()
+{
+}
+
+MessageManager::~MessageManager()
+{
+}
+
 void MessageManager::AddMessage(HWND window, UINT message)
 {
 	Lock lock(m_cs);
