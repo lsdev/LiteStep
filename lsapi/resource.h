f013c0010d2029bb4afe5eb692883c76f4d781c4
jugg <jugg>
2009-01-07 19:33:29 +0000

    Regenerated resource scripts with VC6 - no functional change.

--- lsapi/resource.h
+++ lsapi/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
+// Microsoft Developer Studio generated include file.
 // Used by lsapi.rc
 //
 
@@ -10,7 +10,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
