2524b93753cdb36624e012fc92673cd9878255ee
jugg <jugg>
2009-02-23 20:46:31 +0000

    Removed unmaintained Dev-C++ project files.

--- create_dirs.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Creating output directories to work around Dev-C++ bug
-md output
-md output\temp
-md output\temp\utility
-md output\temp\hook
-md output\temp\lsapi
-md output\temp\litestep
